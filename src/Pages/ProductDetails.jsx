2df06bb5a33598e05da854cd9adceee208a6f399
R. P. Gangawane <163027220+Raj-1907@users.noreply.github.com>
2026-08-08 16:52:09 +0530

    Fix import path for CartContext in ProductDetails

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { CartContext } from '../context/CartContext';
+import { CartContext } from '../Context/CartContext';
 
 const ProductDetails = () => {
   const { id } = useParams();
